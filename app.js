149b0ab89c929bfc920b3fc61a7090b0363dea89
vupa2 <anhvu.pa96@gmail.com>
2021-03-08 14:24:19 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+document.querySelector('button').addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const input = document.querySelector('input').value;
+  const from = document.querySelector('#from').value;
+  const to = document.querySelector('#to').value;
+
+  result = (input * to / from).toFixed(5);
+  document.querySelector('#result').innerHTML = `Result: ${result}`;
+})
